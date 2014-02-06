4782a23c812d7b6e430d36db1eb7bf6e64f5f364
Jeremy Nelson <jermnelson.gmail.com>
2014-02-06 09:48:53 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Flask-BIBFRAME
+The Flask-BIBFRAME extension provides Linked Data support for Flask web 
+applications like Colorado College's [TIGER Catalog](https://github.com/jermnelson/tiger-catalog).
+
+This project's source code is licensed under the [MIT](http://opensource.org/licenses/MIT) license. 
---
+++ setup.py
@@ -0,0 +1,46 @@
+"""
+Flask-BIBFRAME
+--------------
+
+This Flask extension provides  for `BIBFRAME <http://bibframe.org>`_ Linked
+Data support in Flask applications.
+"""
+__author__ = "Jeremy Nelson"
+__version_info__ = ('0', '0', '1')
+__version__ = '.'.join(__version_info__)
+
+from setuptools import find_packages, setup
+
+
+
+setup(
+    name='Flask-FedoraCommons',
+    version=__version__,
+    url='http://github.com/jermnelson/flask_bibframe',
+    license='MIT License',
+    author=__author__,
+    author_email='jermnelson@gmail.com',
+    description='Library for BIBFRAME Linked Data support in Flask apps',
+    long_description=__doc__,
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'python-dateutil',
+        'Flask',
+        'poster',
+        'rdflib'
+    ],
+    classifiers=[
+        'Framework :: Flask',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+
+    ]
+)
